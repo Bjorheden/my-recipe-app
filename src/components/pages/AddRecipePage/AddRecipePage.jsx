607fcc4cbd37b41d8044405d3d0a2393bc263322
Joakim Persson <joape526@student.liu.se>
2023-10-22 12:38:19 +0200

    AddRecipe now adds recipes in the database

--- src/components/pages/AddRecipePage/AddRecipePage.jsx
+++ src/components/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,20 +1,31 @@
 // AddRecipePage.js
 import React from "react";
 import AddRecipeForm from "../../organisms/AddRecipeForm/AddRecipeForm";
-import './AddRecipePage.css'
+import "./AddRecipePage.css";
 
 const AddRecipePage = ({ onAddRecipe }) => {
     const handleAddRecipe = (recipeData) => {
-        // This is where you can handle adding the recipe to your data store.
-        // You can call the `onAddRecipe` function passed from your parent component.
-        // Example:
-        //onAddRecipe(recipeData);
-        console.log(recipeData);
+        fetch("http://localhost:5000/api/recipes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(recipeData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Recipe added successfully:", data);
+                // Optionally, you can add code to navigate to a different page or show a success message to the user.
+            })
+            .catch((error) => {
+                console.error("Error adding recipe:", error);
+                // Handle errors here, e.g., display an error message to the user.
+            });
     };
 
     return (
         <div className="add-recipe-container">
-            <AddRecipeForm onAddRecipe={handleAddRecipe}/>
+            <AddRecipeForm onAddRecipe={handleAddRecipe} />
         </div>
     );
 };
